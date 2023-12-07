01ad957461e9b174d57ceafba297e5e5b60a0bb9
Jakob Ristner <jakob.o.ristner@gmail.com>
2023-12-07 12:16:54 +0100

    part 2

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,15 +1,14 @@
-use std::{
-    cmp::Ordering,
-    collections::{HashMap, HashSet},
-    iter::zip,
-};
+use std::{cmp::Ordering, collections::HashMap, iter::zip};
 
 type Hand = (Vec<u32>, u32, u32);
 
 fn main() {
-    let hands = parse();
-    let sol1 = part_1(&mut hands.clone());
+    let mut hands1 = parse_1();
+    let sol1 = part_1(&mut hands1);
+    let mut hands2 = parse_2();
+    let sol2 = part_1(&mut hands2);
     println!("{}", sol1);
+    println!("{}", sol2);
 }
 
 fn part_1(hands: &mut Vec<Hand>) -> u32 {
@@ -39,6 +38,17 @@ fn cmp_hand((cards_1, score_1, _): &Hand, (cards_2, score_2, _): &Hand) -> Order
     }
 }
 
+fn get_card_score_part2(ch: char) -> u32 {
+    match ch {
+        'A' => 13,
+        'K' => 12,
+        'Q' => 11,
+        'J' => 0,
+        'T' => 10,
+        c => c.to_digit(10).expect("not a card"),
+    }
+}
+
 fn get_card_score(ch: char) -> u32 {
     match ch {
         'A' => 14,
@@ -61,6 +71,20 @@ fn get_occurences(cards: &Vec<u32>) -> Vec<u32> {
     occ
 }
 
+fn get_hand_score_part_2(cards: &Vec<u32>) -> u32 {
+    let mut out: Vec<u32> = Vec::new();
+    for i in 1..=14 {
+        let mut test = cards.clone();
+        test.iter_mut().for_each(|x| {
+            if *x == 0 {
+                *x = i
+            }
+        });
+        out.push(get_hand_score(&test));
+    }
+    *out.iter().max().unwrap()
+}
+
 fn get_hand_score(cards: &Vec<u32>) -> u32 {
     let occ = get_occurences(cards);
     match &occ[0..5] {
@@ -74,7 +98,21 @@ fn get_hand_score(cards: &Vec<u32>) -> u32 {
     }
 }
 
-fn parse() -> Vec<Hand> {
+fn parse_2() -> Vec<Hand> {
+    include_str!("../input.txt")
+        .lines()
+        .map(|line| {
+            let (cards_raw, bid_raw) = line.split_once(" ").unwrap();
+            let cards: Vec<u32> = cards_raw.chars().map(get_card_score_part2).collect();
+            let bid: u32 = bid_raw.parse().unwrap();
+            let score = get_hand_score_part_2(&cards);
+
+            (cards, score, bid)
+        })
+        .collect()
+}
+
+fn parse_1() -> Vec<Hand> {
     include_str!("../input.txt")
         .lines()
         .map(|line| {
